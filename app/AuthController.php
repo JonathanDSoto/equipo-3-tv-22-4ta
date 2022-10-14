700a26fd028644a5fd89bc00269d973db379a763
tezjavi@gmail.com <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-10-14 15:13:51 -0600

    Funcionamiento del Login y Register

--- app/AuthController.php
+++ app/AuthController.php
@@ -1,6 +1,5 @@
 <?php
     include_once "config.php";
-    include_once "AuthContoller.php";
 
 	if (isset($_POST["action"])) {
 		if(isset($_POST["super_token"]) && $_POST["super_token"] == $_SESSION["super_token"]) {
@@ -13,6 +12,18 @@
 					$authController = new AuthController();
 					$authController->login($email,$password);
 				break;
+                case 'register':
+                    $name = strip_tags($_POST['name']);
+                    $lastname = strip_tags($_POST['lastname']);
+                    $email = strip_tags($_POST['email']);
+                    $phone_number = strip_tags($_POST['phone_number']);
+                    $created_by = strip_tags($_POST['created_by']);
+                    $role = strip_tags($_POST['role']);
+                    $password = strip_tags($_POST['password']);
+
+                    $authcontroller = new AuthController();
+                    $authcontroller->register($name, $lastname, $email, $phone_number, $created_by, $role, $password);
+                break;
 			}
 		}
 
@@ -46,7 +57,7 @@
 
 			if (isset($response->code) && $response->code > 0) {
 				
-				session_start();
+                echo 'Si jalo mi apa';
 
 				$_SESSION['id'] = $response->data->id;
 				$_SESSION['name'] = $response->data->name;
@@ -55,8 +66,7 @@
 				$_SESSION['role'] = $response->data->role;
 				$_SESSION['token'] = $response->data->token;
 
-
-				header("Location:".BASE_PATH."products?success");
+                header("Location:".BASE_PATH."products?success");
 
 			}else{
 
@@ -64,6 +74,38 @@
 			}
 
 		}
+
+        public function register ($name, $lastname, $email, $phone_number, $created_by, $role, $password){
+            $curl = curl_init();
+            
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => 'https://crud.jonathansoto.mx/api/register',
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => '',
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => 'POST',
+              CURLOPT_POSTFIELDS => array(
+                'name' => $name,
+                'lastname' => $lastname,
+                'email' => $email,
+                'phone_number' =>$phone_number,
+                'created_by' =>  $created_by,
+                'role' => $role,
+                'password' => $password,
+                'profile_photo_file'=>new CURLFILE($_FILES['profile_photo_file']['tmp_name'])),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            $response = json_decode($response);
+            if (isset($response->code) &&  $response->code > 0) {
+                $this->login($email, $password);
+            } else {
+                header("Location:".BASE_PATH."?error=true");
+            }
+        }
 
 	}
     
--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<!-- <h1><?= $_SERVER['HTTP_HOST']?></h1> -->
+<?php
+include_once "app/config.php"
+?>
+<h1>Formulario</h1>
+<!-- <form action="app/AuthController.php" method="post">
+    <input type="email" name="email"><br><br>
+    <input type="password" name="password"><br><br>
+    <input type="hidden" name="action" value="access">
+    <input type="hidden" name="super_token" value="<?= $_SESSION['super_token'] ?>">
+
+
+    <input type="submit" value="submit">
+</form> -->
+
+<form enctype="multipart/form-data" action="app/AuthController.php" method="post">
+    
+    <input type="text" name="name"><br><br>
+    <input type="text" name="lastname"><br><br>
+    <input type="email" name="email"><br><br>
+    <input type="number" name="phone_number"><br><br>
+    <input type="text" name="created_by"><br><br>
+    <input type="text" name="role"><br><br>
+    <input type="password" name="password"><br><br>
+    <input type="file" name="profile_photo_file"><br><br>
+    <input type="hidden" name="action" value="register">
+    <input type="hidden" name="super_token" value="<?= $_SESSION['super_token'] ?>">
+
+    <input type="submit" value="submit">
+</form>
+
