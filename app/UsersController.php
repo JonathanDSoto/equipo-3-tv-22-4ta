72f42b4048d8487b0e448e99064689e4e240aeb8
tezjavi@gmail.com <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-10-14 20:36:07 -0600

    Agregar Usuarios (en progreso)

--- app/UsersController.php
+++ app/UsersController.php
@@ -5,35 +5,46 @@
 		if(isset($_POST["super_token"]) && $_POST["super_token"] == $_SESSION["super_token"]) {
 			switch($_POST['action'])
 			{
-				case 'access':
-					$email = strip_tags($_POST['email']);
-					$password = strip_tags($_POST['password']);
-	
-					$authController = new AuthController();
-					$authController->login($email,$password);
-				break;
-                case 'register':
-                    $name = strip_tags($_POST['name']);
-                    $lastname = strip_tags($_POST['lastname']);
-                    $email = strip_tags($_POST['email']);
-                    $phone_number = strip_tags($_POST['phone_number']);
-                    $created_by = strip_tags($_POST['created_by']);
-                    $role = strip_tags($_POST['role']);
-                    $password = strip_tags($_POST['password']);
-
-                    $authcontroller = new AuthController();
-                    $authcontroller->register($name, $lastname, $email, $phone_number, $created_by, $role, $password);
-                break;
-                case 'recovery':
-                    $authcontroller = new AuthController();
-                    $authcontroller->recoveryPass($_POST["email"]);
-                break;
-                case 'logout':
-                    $authcontroller = new AuthController();
-                    $authcontroller->logout($_POST["email"]);
+				case 'newUser':
+                    $userController = new UserController();
+                    $userController->createUser(
+                        $_POST['name'], $_POST['lastname'], $_POST['email'], $_POST['phone_number'], $_POST['created_by'], $_POST['role'], $_POST['password']);
                 break;
 			}
 		}
-
 	}
+    Class UserController {
+        public function createUser($name, $lastname, $email, $phone_number, $created_by, $role, $password){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array(
+                'name' => $name,
+                'lastname' => $lastname,
+                'email' => $email,
+                'phone_number' => $phone_number,
+                'created_by' => $created_by,
+                'role' => $role,
+                'password' => $password,
+                'profile_photo_file'=> new CURLFILE($_FILES['profile_photo_file']['tmp_name'])),
+
+                CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer' .$_SESSION['token'],
+            ),
+            ));
+    
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            echo $response;
+        }
+    
+    }
 ?>
--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 include_once "app/config.php"
 ?>
 <h1>Formulario</h1>
-<form action="app/AuthController.php" method="post">
+<!-- <form action="app/AuthController.php" method="post">
     <input type="email" name="email"><br><br>
     <input type="password" name="password"><br><br>
     <input type="hidden" name="action" value="access">
     <input type="hidden" name="super_token" value="<?= $_SESSION['super_token'] ?>">
 
     <input type="submit" value="submit">
-</form>
+</form> -->
 
 <!-- <form enctype="multipart/form-data" action="app/AuthController.php" method="post">
     
@@ -33,9 +33,23 @@ include_once "app/config.php"
     <input type="hidden" name="super_token" value="<?= $_SESSION['super_token'] ?>">
     <input type="submit" value="submit">
 </form> -->
-<form  enctype="multipart/form-data" action="app/AuthController.php" method="post">
+<!-- <form  enctype="multipart/form-data" action="app/AuthController.php" method="post">
     <input type="submit" value="Cerrar Sesion">
     <input type="hidden" name="action" value="logout">
     <input type="hidden" name="email" value="<?= $_SESSION['email'] ?>">
     <input type="hidden" name="super_token" value="<?= $_SESSION['super_token'] ?>">
+</form> -->
+
+<form enctype="multipart/form-data" action="app/UsersController.php" method="post">
+    <input type="text" name="name"><br><br>
+    <input type="text" name="lastname"><br><br>
+    <input type="email" name="email"><br><br>
+    <input type="number" name="phone_number"><br><br>
+    <input type="text" name="created_by"><br><br>
+    <input type="text" name="role"><br><br>
+    <input type="password" name="password"><br><br>
+    <input type="file" name="profile_photo_file"><br><br>
+    <input type="hidden" name="action" value="newUser">
+    <input type="hidden" name="super_token" value="<?= $_SESSION['super_token'] ?>">
+    <input type="submit" value="Agregar">
 </form>
