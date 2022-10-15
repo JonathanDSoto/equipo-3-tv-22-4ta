061fa076883270cd01b9d7f5ae04842c374f7d1e
tezjavi@gmail.com <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-10-15 14:21:48 -0600

    Controlador de borrar usuario(no probado)

--- app/UsersController.php
+++ app/UsersController.php
@@ -25,6 +25,11 @@
                     $usercontroller = new UserController();
                     $usercontroller->editUser($name, $lastname, $email, $phone_number, $password, $id);
                 break;
+                case 'deleteUser': 
+                    $idEl = $_POST['idEliminar'];
+                    $productController = new UserController();
+                    $productController -> deleteUser($idEl);
+                break;
 			}
 		}
 	}
@@ -119,6 +124,38 @@
 
 				header("Location:../products?error");
 			}
+        }
+        public function deleteUser($id){
+
+            $curl = curl_init($id);
+            var_dump($id);
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users/'.$id,
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => '',
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => 'DELETE',
+              CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer'.$_SESSION['token'],
+              ),
+            ));
+            
+            $response = curl_exec($curl);
+            
+            curl_close($curl);
+
+            $response = json_decode($response);
+    
+            if( isset($response->code) &&  $response->code > 0) {
+                $var = $response->message;
+                header ("Location:../view/productos");
+            } else{
+                return $response;
+            }
+            
         }
     }
 ?>
