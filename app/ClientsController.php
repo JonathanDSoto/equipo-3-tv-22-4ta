cf3987b7ad81b8f803d990de51444fb4d815947d
tezjavi@gmail.com <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-10-16 13:36:13 -0600

    Se agrego el controlador de clientes con eliminar, agregar, editar, y obtener los clientes

--- app/ClientsController.php
+++ app/ClientsController.php
@@ -6,16 +6,27 @@
 			switch($_POST['action'])
 			{
 				case 'newClients':
-                    $name = strip_tags($_POST['name']);
-                    $email = strip_tags($_POST['email']);
-                    $phone_number = strip_tags($_POST['phone_number']);
-                    $password = strip_tags($_POST['password']);
-                    $is_suscribed = strip_tags($_POST['is_suscribed']);
-                    $level_id = strip_tags($_POST['level_id']);
-                    $clientscontroller = new ClientsController();
-                    $usercontroller->createUser($name, $email, $phone_number, $password,$is_suscribed, $level_id);
-                break;
-            }
+          $name = strip_tags($_POST['name']);
+          $email = strip_tags($_POST['email']);
+          $phone_number = strip_tags($_POST['phone_number']);
+          $password = strip_tags($_POST['password']);
+          $is_suscribed = strip_tags($_POST['is_suscribed']);
+          $level_id = strip_tags($_POST['level_id']);
+          $clientscontroller = new ClientsController();
+          $usercontroller->createUser($name, $email, $phone_number, $password,$is_suscribed, $level_id);
+        break;
+				case 'editClients':
+          $name = strip_tags($_POST['name']);
+          $email = strip_tags($_POST['email']);
+          $phone_number = strip_tags($_POST['phone_number']);
+          $password = strip_tags($_POST['password']);
+          $is_suscribed = strip_tags($_POST['is_suscribed']);
+          $level_id = strip_tags($_POST['level_id']);
+          $id = strip_tags($_POST['id']);
+          $clientscontroller = new ClientsController();
+          $usercontroller->editClients($name, $email, $phone_number, $password,$is_suscribed, $level_id, $id);
+        break;
+      }
 		}
 	}
    Class ClientsController {
@@ -40,30 +51,103 @@
             echo $response;
         }
         public function editClients($name, $email, $phone_number, $password,$is_suscribed, $level_id, $id){
+          $curl = curl_init();
+          
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_POSTFIELDS => 'name='.$name.'&email='.$email.'&password='.$password.'&phone_number='.$phone_number.'&is_suscribed=1'.$is_suscribed.'&level_id='.$level_id.'&id='.$id,
+            CURLOPT_HTTPHEADER => array(
+              'Authorization: Bearer'.$_SESSION['token'],
+              'Content-Type: application/x-www-form-urlencoded',
+            ),
+          ));
+          
+          $response = curl_exec($curl);
+          
+          curl_close($curl);
+          echo $response;
+        }
+        public function deleteClients($id){
+          $curl = curl_init();
+          
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients/'.$id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'DELETE',
+            CURLOPT_HTTPHEADER => array(
+              'Authorization: Bearer '.$_SESSION['token']
+            ),
+          ));
+          $response = curl_exec($curl);
+          
+          curl_close($curl);
 
-            $curl = curl_init();
-            
-            curl_setopt_array($curl, array(
-              CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients',
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_ENCODING => '',
-              CURLOPT_MAXREDIRS => 10,
-              CURLOPT_TIMEOUT => 0,
-              CURLOPT_FOLLOWLOCATION => true,
-              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-              CURLOPT_CUSTOMREQUEST => 'PUT',
-              CURLOPT_POSTFIELDS => 'name='.$name.'&email='.$email.'&password='.$password.'&phone_number='.$phone_number.'&is_suscribed=1'.$is_suscribed.'&level_id='.$level_id.'&id='.$id,
-              CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer'.$_SESSION['token'],
-                'Content-Type: application/x-www-form-urlencoded',
-              ),
-            ));
-            
-            $response = curl_exec($curl);
-            
-            curl_close($curl);
-            echo $response;
-            
+          $response = json_decode($response);
+  
+          if( isset($response->code) &&  $response->code > 0) {
+              $var = $response->message;
+          } else{
+              return $response;
+          }
+        }
+        public function getAllClients(){
+          $curl = curl_init();
+          
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+              'Authorization: Bearer '.$_SESSION['token'],
+            ),
+          ));
+          
+          $response = curl_exec($curl);
+          
+          curl_close($curl);
+          echo $response;
+          
+        }
+        public function getEspecificClients($id){
+
+          $curl = curl_init();
+
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/clients/'.$id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+              'Authorization: Bearer '.$_SESSION,
+            ),
+          ));
+
+          $response = curl_exec($curl);
+
+          curl_close($curl);
+          echo $response;
+
         }
     }
 ?>
--- app/UsersController.php
+++ app/UsersController.php
@@ -6,30 +6,30 @@
 			switch($_POST['action'])
 			{
 				case 'newUser':
-                    $name = strip_tags($_POST['name']);
-                    $lastname = strip_tags($_POST['lastname']);
-                    $email = strip_tags($_POST['email']);
-                    $phone_number = strip_tags($_POST['phone_number']);
-                    $password = strip_tags($_POST['password']);
-                    $profile_photo = new CURLFILE($_FILES['profile_photo_file']['tmp_name']);
-                    $usercontroller = new UserController();
-                    $usercontroller->createUser($name, $lastname, $email, $phone_number, $password, $profile_photo);
-                break;
-                case'editUser':
-                    $name = strip_tags($_POST['name']);
-                    $lastname = strip_tags($_POST['lastname']);
-                    $email = strip_tags($_POST['email']);
-                    $phone_number = strip_tags($_POST['phone_number']);
-                    $password = strip_tags($_POST['password']);
-                    $id= strip_tags($_POST['id']);
-                    $usercontroller = new UserController();
-                    $usercontroller->editUser($name, $lastname, $email, $phone_number, $password, $id);
-                break;
-                case 'deleteUser': 
-                    $idEl = $_POST['idEliminar'];
-                    $productController = new UserController();
-                    $productController -> deleteUser($idEl);
-                break;
+          $name = strip_tags($_POST['name']);
+          $lastname = strip_tags($_POST['lastname']);
+          $email = strip_tags($_POST['email']);
+          $phone_number = strip_tags($_POST['phone_number']);
+          $password = strip_tags($_POST['password']);
+          $profile_photo = new CURLFILE($_FILES['profile_photo_file']['tmp_name']);
+          $usercontroller = new UserController();
+          $usercontroller->createUser($name, $lastname, $email, $phone_number, $password, $profile_photo);
+      break;
+      case'editUser':
+          $name = strip_tags($_POST['name']);
+          $lastname = strip_tags($_POST['lastname']);
+          $email = strip_tags($_POST['email']);
+          $phone_number = strip_tags($_POST['phone_number']);
+          $password = strip_tags($_POST['password']);
+          $id= strip_tags($_POST['id']);
+          $usercontroller = new UserController();
+          $usercontroller->editUser($name, $lastname, $email, $phone_number, $password, $id);
+      break;
+      case 'deleteUser': 
+          $idEl = $_POST['idEliminar'];
+          $productController = new UserController();
+          $productController -> deleteUser($idEl);
+      break;
 			}
 		}
 	}
@@ -65,61 +65,59 @@
             }
         }
         public function getAllUsers() {
-            $curl = curl_init();
-            
-            curl_setopt_array($curl, array(
-              CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users',
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_ENCODING => '',
-              CURLOPT_MAXREDIRS => 10,
-              CURLOPT_TIMEOUT => 0,
-              CURLOPT_FOLLOWLOCATION => true,
-              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-              CURLOPT_CUSTOMREQUEST => 'GET',
-              CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer '.$_SESSION['token'],
-              ),
-            ));
-            
-			$response = curl_exec($curl); 
-			curl_close($curl);
-			$response = json_decode($response);
+          $curl = curl_init();
+          
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+              'Authorization: Bearer '.$_SESSION['token'],
+            ),
+          ));
+          $response = curl_exec($curl); 
+          curl_close($curl);
+          $response = json_decode($response);
 
-			if (isset($response->code) && $response->code>0) {
-				return $response->data;
-				
-			}else{
-				return array();
-			}
-        }
+          if (isset($response->code) && $response->code>0) {
+            return $response->data;
+            
+          }else{
+            return array();
+          }
+            }
         public function getEspecificUser($id){
-            $curl = curl_init();
-            
-            curl_setopt_array($curl, array(
-              CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users/'.$id,
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_ENCODING => '',
-              CURLOPT_MAXREDIRS => 10,
-              CURLOPT_TIMEOUT => 0,
-              CURLOPT_FOLLOWLOCATION => true,
-              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-              CURLOPT_CUSTOMREQUEST => 'GET',
-              CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer '.$_SESSION['token'] ,
-              ),
-            ));
-            
-            $response = curl_exec($curl);
-            curl_close($curl);
-			$response = json_decode($response);
+          $curl = curl_init();
+          
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users/'.$id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_HTTPHEADER => array(
+              'Authorization: Bearer '.$_SESSION['token'] ,
+            ),
+          ));
+          
+          $response = curl_exec($curl);
+          curl_close($curl);
+          $response = json_decode($response);
 
-			if (isset($response->code) && $response->code>0) {
-				return $response->data;
-				
-			}else{
-				return array();
-			}
+          if (isset($response->code) && $response->code>0) {
+            return $response->data;
             
+          }else{
+            return array();
+          }
         }
         public function editUser($name, $lastname, $email, $phone_number, $password, $id){
             $curl = curl_init();
@@ -155,36 +153,34 @@
 			}
         }
         public function deleteUser($id){
+          $curl = curl_init($id);
+          var_dump($id);
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users/'.$id,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'DELETE',
+            CURLOPT_HTTPHEADER => array(
+              'Authorization: Bearer'.$_SESSION['token'],
+            ),
+          ));
+          
+          $response = curl_exec($curl);
+          
+          curl_close($curl);
 
-            $curl = curl_init($id);
-            var_dump($id);
-            curl_setopt_array($curl, array(
-              CURLOPT_URL => 'https://crud.jonathansoto.mx/api/users/'.$id,
-              CURLOPT_RETURNTRANSFER => true,
-              CURLOPT_ENCODING => '',
-              CURLOPT_MAXREDIRS => 10,
-              CURLOPT_TIMEOUT => 0,
-              CURLOPT_FOLLOWLOCATION => true,
-              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-              CURLOPT_CUSTOMREQUEST => 'DELETE',
-              CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer'.$_SESSION['token'],
-              ),
-            ));
-            
-            $response = curl_exec($curl);
-            
-            curl_close($curl);
-
-            $response = json_decode($response);
-    
-            if( isset($response->code) &&  $response->code > 0) {
-                $var = $response->message;
-                //header ("Location:../view/productos");
-            } else{
-                return $response;
-            }
-            
+          $response = json_decode($response);
+  
+          if( isset($response->code) &&  $response->code > 0) {
+              $var = $response->message;
+              //header ("Location:../view/productos");
+          } else{
+              return $response;
+          }
         }
     }
 ?>
