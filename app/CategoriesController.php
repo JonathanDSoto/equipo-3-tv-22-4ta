d20e053a87abff38a9502663b889d5b04269a731
tezjavi@gmail.com <javiermurillocota@MacBook-Pro-de-Javier.local>
2022-10-16 22:16:55 -0600

    Se agrego contenido al controlador de Categorias como el editar y el crear

---
+++ app/CategoriesController.php
@@ -0,0 +1,91 @@
+<?php 
+	include_once 'config.php';
+	if (isset($_POST["action"])) {
+		if(isset($_POST["super_token"]) && $_POST["super_token"] == $_SESSION["super_token"]) {
+			switch($_POST['action'])
+			{
+				case 'create':
+					$name = strip_tags($_POST['name']);
+					$description = strip_tags($_POST['description']);
+					$slug = strip_tags($_POST['slug']);
+					$category_id = strip_tags($_POST['category_id']);
+					
+					$productsController = new ProductsController();
+					$productsController->createProduct($name,$slug,$description,$features,$category_id);
+				break;
+                case 'editCategory':
+                    $id = strip_tags($_POST['id']);
+                    $productsController = new ProductsController();
+					$productsController->createProduct($name,$slug,$description,$features,$category_id);
+                break;
+			}
+		}
+	}
+
+	Class CategoryController
+	{
+		public function createCategory($name,$slug,$description,$category_id)
+		{
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/categories',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array('name' => $name,'description' => $description,'slug' => $slug,'category_id' => $category_id),
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$_SESSION['token']
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            echo $response;
+
+			$response = curl_exec($curl); 
+			curl_close($curl);
+			$response = json_decode($response); 
+
+			if (isset($response->code) && $response->code > 0) { 
+
+				// header("Location:../products?success");
+
+			}else{
+
+				// header("Location:../products?error");
+			}
+		}
+        public function editCategory($id, $name, $description, $slug, $category_id){
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/categories',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'PUT',
+            CURLOPT_POSTFIELDS => 'id='.$id.'&name='.$name.'&description='.$description.'&slug='.$slug.'&category_id='.$category_id,
+            CURLOPT_HTTPHEADER => array(
+                'Authorization: Bearer '.$_SESSION['token'],
+                'Content-Type: application/x-www-form-urlencoded'
+            ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            echo $response;
+
+        }
+		
+	}
+?>
