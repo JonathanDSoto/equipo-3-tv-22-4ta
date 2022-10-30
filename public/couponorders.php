2b41c7391fca9c5bcc0353818697f931869b62b8
Osiris <tompsom98@gmail.com>
2022-10-30 02:45:07 -0700

    6to modulo terminado

---
+++ public/couponorders.php
@@ -0,0 +1,156 @@
+<?php
+include "../app/config.php";
+include '../assets/layouts/includes.php';
+include "../app/ClientsController.php";
+$clientController = new ClientsController();
+$clients = $clientController->getAllClients();
+?>
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Menu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../assets/images/favicon.ico">
+
+    <!-- jsvectormap css -->
+    <link href="../assets/libs/jsvectormap/css/jsvectormap.min.css" rel="stylesheet" type="text/css" />
+
+    <!--Swiper slider css-->
+    <link href="../assets/libs/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Layout config Js -->
+    <script src="../assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="../assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="../assets/css/custom.min.css" rel="stylesheet" type="text/css" />
+
+
+</head>
+
+<body>
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+        <!-- Inicia navbar -->
+        <?php include '../assets/layouts/navbar.template.php' ?>
+        <!-- Termina navbar -->
+
+        <!-- ========== App Menu ========== -->
+
+        <?php include '../assets/layouts/sidebar.template.php' ?>
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid">
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                <h4 class="mb-sm-0">Ordenes</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end page title -->
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card">
+
+                                <div class="card-body">
+                                    <div id="customerList">
+                                        <div class="table-responsive table-card mt-3 mb-1">
+                                            <table class="table align-middle table-nowrap" id="customerTable">
+                                                <thead class="table-light">
+                                                    <tr> 
+                                                        <th class="" data-sort="id">ID</th>
+                                                        <th class="" data-sort="folio">Folio</th>
+                                                        <th class="" data-sort="total">Total</th>
+                                                        <th class="" data-sort="action">Action</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody class="list form-check-all">
+                                                    <tr>
+                                                        <td class="id" style="display:none;"><a href="javascript:void(0);" class="fw-medium link-primary">#VZ2101</a></td>
+                                                        <td class="id">1</td>
+                                                        <td class="folio">82718</td>
+                                                        <td class="total">9599.969</td>
+                                                        <td>
+                                                            <div class="d-flex gap-2">
+                                                                <div class="View">
+                                                                    <a href="detailsorders.php">
+                                                                        <button class="btn btn-sm btn-primary edit-item-btn" data-bs-toggle="modal" data-bs-target="">Ver</button>
+                                                                    </a>
+                                                                </div>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+
+                                </div><!-- end card -->
+                            </div>
+                            <!-- end col -->
+                        </div>
+                        <!-- end col -->
+                    </div>
+                    <!-- end row -->
+
+                </div>
+                <!-- container-fluid -->
+            </div>
+            <!-- End Page-content -->
+        </div>
+        <!-- end main content-->
+    </div>
+    <?php include '../assets/layouts/footer.template.php' ?>
+    <!-- END layout-wrapper -->
+
+
+
+    <!--start back-to-top-->
+    <button onclick="topFunction() " class="btn btn-danger btn-icon " id="back-to-top ">
+        <i class="ri-arrow-up-line "></i>
+    </button>
+    <!--end back-to-top-->
+
+    <!--preloader-->
+    
+
+    <!-- JAVASCRIPT -->
+    <?php include '../assets/layouts/js.template.php' ?>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+    
+    <!-- apexcharts -->
+    <script src="../assets/libs/apexcharts/apexcharts.min.js "></script>
+
+    <!-- Vector map-->
+    <script src="../assets/libs/jsvectormap/js/jsvectormap.min.js "></script>
+    <script src="../assets/libs/jsvectormap/maps/world-merc.js "></script>
+
+    <!--Swiper slider js-->
+    <script src="../assets/libs/swiper/swiper-bundle.min.js "></script>
+
+    <!-- Dashboard init -->
+    <script src="../assets/js/pages/dashboard-ecommerce.init.js "></script>
+
+    <!-- App js -->
+    <script src="../assets/js/app.js "></script>
+</body>
+
+
+</html>
---
+++ public/detailscupones.php
@@ -0,0 +1,158 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Menu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../assets/images/favicon.ico">
+    <!-- jsvectormap css -->
+    <link href="../assets/libs/jsvectormap/css/jsvectormap.min.css" rel="stylesheet" type="text/css" />
+    <!--Swiper slider css-->
+    <link href="../assets/libs/swiper/swiper-bundle.min.css" rel="stylesheet" type="text/css" />
+    <!-- Layout config Js -->
+    <script src="../assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="../assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="../assets/css/custom.min.css" rel="stylesheet" type="text/css" />
+
+
+</head>
+
+<body>
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+        <!-- Inicia navbar -->
+        <?php include '../assets/layouts/navbar.template.php' ?>
+        <!-- Termina navbar -->
+        <!-- Inicia sidebar -->
+        <?php include '../assets/layouts/sidebar.template.php' ?>
+        <!-- Termina sidebar -->
+        <!-- ========== App Menu ========== -->
+        <div class="main-content">
+            <div class="page-content ">
+                <div class="profile-foreground position-relative mx-n4 mt-n4">
+                    <div class="profile-wid-bg">
+                        <img src="../assets/images/profile-bg.jpg" alt="" class="profile-wid-img" />
+                    </div>
+                </div>
+                <div class="pt-4 mb-4 mb-lg-3 pb-lg-4">
+                    <div class="row g-4 ">
+                        <div class="col-auto">
+                            <div class="avatar-lg">
+                                
+                            </div>
+                        </div>
+                        <!--end col-->
+                        <div class="col">
+                            <div class="p-2">
+                                
+                            </div>
+                        </div>
+                        <!--end col-->
+                    </div>
+                    <!--end row-->
+                </div>
+                <div class="row">
+                    <!-- Tab panes -->
+                    <div class="tab-content pt-4 text-muted ">
+                        <div class="tab-pane active" id="overview-tab" role="tabpanel">
+                            <div class="row">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title mb-3">Info</h5>
+                                        <div class="table-responsive">
+                                            <table class="table table-borderless mb-0">
+                                                <tbody>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Coupon Name :</th>
+                                                        <td class="text-muted">15% off</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Percentage discount :</th>
+                                                        <td class="text-muted">15</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Amount discount :</th>
+                                                        <td class="text-muted">0</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Min amount required :</th>
+                                                        <td class="text-muted">500</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Min product required :</th>
+                                                        <td class="text-muted">5</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Max uses :</th>
+                                                        <td class="text-muted">100</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Start date :</th>
+                                                        <td class="text-muted">2022-10-29</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">End date :</th>
+                                                        <td class="text-muted">2022-10-30</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th class="ps-0" scope="row">Couponable type:</th>
+                                                        <td class="text-muted">Cupon por porcentaje</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- .row-->
+            </div>
+        </div>
+    </div>
+    <?php include '../assets/layouts/footer.template.php' ?>
+    <!-- END layout-wrapper -->
+
+
+
+    <!--start back-to-top-->
+    <button onclick="topFunction() " class="btn btn-danger btn-icon " id="back-to-top ">
+        <i class="ri-arrow-up-line "></i>
+    </button>
+
+    <!--end back-to-top-->
+
+
+    <!-- JAVASCRIPT -->
+    <?php include '../assets/layouts/js.template.php' ?>
+
+    <!-- apexcharts -->
+    <script src="../assets/libs/apexcharts/apexcharts.min.js "></script>
+
+    <!-- Vector map-->
+    <script src="../assets/libs/jsvectormap/js/jsvectormap.min.js "></script>
+    <script src="../assets/libs/jsvectormap/maps/world-merc.js "></script>
+
+    <!--Swiper slider js-->
+    <script src="../assets/libs/swiper/swiper-bundle.min.js "></script>
+
+    <!-- Dashboard init -->
+    <script src="../assets/js/pages/dashboard-ecommerce.init.js "></script>
+
+    <!-- App js -->
+    <script src="../assets/js/app.js "></script>
+</body>
+
+</html>
